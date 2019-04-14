d0ce39e543488420c4c8a1a746cee0bf4ec3f33f
Loui Sakaki <e1352207@outlook.jp>
2019-04-14 22:09:20 +0900

    updated

--- clean_up.bat
+++ clean_up.bat
@@ -1,9 +1,24 @@
 @echo off
 setlocal
 
-pushd "%~dp0"
+:: https://qiita.com/hkuno/items/e7fedc20a61979aa6078
+:: https://qiita.com/sawa_tsuka/items/8edf3d3d33a0ae86cb5c
+
+if "%1"=="" (
+    set work_dir=.
+
+) else (
+    set work_dir=%~f1
+)
+
+pushd %work_dir%
+    if not %cd% == %work_dir% (
+        goto :EOF
+    )
+
     del *~
     del *.hi
     del *.o
     del *.exe
+
 popd
