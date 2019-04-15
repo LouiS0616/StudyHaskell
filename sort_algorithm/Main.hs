e8faaf96cf3b54e5e81c38796ce10a62bbcf93bd
Loui Sakaki <e1352207@outlook.jp>
2019-04-15 18:06:34 +0900

    updated

--- sort_algorithm/Main.hs
+++ sort_algorithm/Main.hs
@@ -1,11 +1,31 @@
+import System.Random (randomRIO)
+
+import BubbleSort1
+import BubbleSort2
+import InsertSort
+import MergeSort
+import QuickSort
+import SelectSort1
+import SelectSort2
+
+-- Ref: https://stackoverflow.com/questions/30740366/list-with-random-numbers-in-haskell
+randomList :: Int -> IO([Int])
+randomList 0 = return []
+randomList n = do
+    r  <- randomRIO (1,100)
+    rs <- randomList (n-1)
+    return (r:rs)
 
 --
 --
 main = do
-    let list = [4, 6, 9, 8, 3, 5, 1, 7, 2]
+    list <- randomList 10
+    print list
 
-    print $ isort  list
-    print $ bsort1 list
-    print $ bsort2 list
-    print $ msort  list
-    print $ qsort  list
+    print $ BubbleSort1.bsort list
+    print $ BubbleSort2.bsort list
+    print $             isort list
+    print $             msort list
+    print $             qsort list
+    print $ SelectSort1.ssort list
+    print $ SelectSort2.ssort list
