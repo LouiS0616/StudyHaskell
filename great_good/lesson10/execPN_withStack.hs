7b0918c2fd7471792b61e727f3d99a3fc9803c70
Loui Sakaki <e1352207@outlook.jp>
2019-11-15 21:38:02 +0900

    updated

--- great_good/lesson10/execPN_withStack.hs
+++ great_good/lesson10/execPN_withStack.hs
@@ -1,17 +1,34 @@
 --
+import qualified Text.Read
+
 import Stack
 
 
 
-execPN_withStack' :: (Read a) => [String] -> Stack a -> (a, Stack a)
-execPN_withStack' (o: os) stack = (read o, stack)
+execPN_withStack' :: (Read a) => [String] -> Stack a -> Stack a
+
+execPN_withStack' [] stack = stack
+
+execPN_withStack' (op: ops) stack =
+    case Text.Read.readMaybe op
+    of Just value -> execPN_withStack' ops (push stack value)
+
+execPN_withStack :: (Read a) => [String] -> Stack a
+execPN_withStack ops = execPN_withStack' ops Stack.Empty
+
+
+--
+applyFunc :: (Num a) => (a -> a -> a) -> Stack a -> (a, Stack a)
+applyFunc f stack = (f op1 op2, newStack2)
+    where
+        (op1, newStack1) = pop stack
+        (op2, newStack2) = pop newStack1
 
 
 --
 main :: IO()
 main = do
     let
-        op = ["3", "1", "+"]
+        ops = ["3", "1"]
     
-    print $ execPN_withStack' op (Stack.Empty :: Stack Int)
-
+    print $ (execPN_withStack ops :: Stack Int)
